3973877a3b5ba3ce746c1a121878c8821b5660ac
Fabien Vanden Bulck <fabien@bulck.be>
2025-06-08 01:00:40 +0200

    Remove ESM build

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'tsup';
 
 export default defineConfig({
   entry: ['src/index.ts'],
-  format: ['esm', 'cjs'],
+  format: ['cjs'],
   splitting: true,
   dts: true,
   sourcemap: true,
